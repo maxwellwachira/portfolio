db15384c0924af3e27dd1994309b0d7fddde9a84
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-28 02:06:40 +0300

    urls and colors constants

---
+++ src/constants/urls.ts
@@ -0,0 +1,7 @@
+export const urls = {
+    baseUrl:'http://localhost:5000',
+    // frontEnd: 'http://localhost:3000',
+    //baseUrl:'https://api.maxwellwachira.com',
+    frontEnd: 'https://maxwellwachira.com',
+    
+}
